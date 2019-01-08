d666a9eb8812d58c5aa3915d2d4f5145dbc992a3
Tal hakmon <33728154+talh6@users.noreply.github.com>
2019-01-08 14:45:37 +0200

    QuizzlerApp

---
+++ Quizzler-iOS12-master/Quizzler/Question.swift
@@ -0,0 +1,20 @@
+//
+//  Question.swift
+//  Quizzler
+//
+//  Created by Tal Hakmon on 1/7/19.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Question {
+    let questionText : String
+    let answer : Bool
+    
+    init(text: String,correctAnswer: Bool){
+        questionText = text
+        answer = correctAnswer
+    }
+}
+
---
+++ Quizzler-iOS12-master/Quizzler/Quizzler-Bridging-Header.m
@@ -0,0 +1,9 @@
+//
+//  Quizzler-Bridging-Header.m
+//  Quizzler
+//
+//  Created by Tal Hakmon on 1/7/19.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+#import "ProgressHUD.h"
