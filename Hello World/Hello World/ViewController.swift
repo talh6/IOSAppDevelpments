27aec4e524e21847b9a25e7f57fdc65128b48b49
Tal hakmon <33728154+talh6@users.noreply.github.com>
2019-01-08 14:43:14 +0200

    Hello world project

---
+++ Hello World/Hello World/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Tal Hakmon on 1/3/19.
+//  Copyright © 2019 Tal Hakmon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+
+    }
+
+
+}
+
